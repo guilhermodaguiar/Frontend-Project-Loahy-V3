ec9685df0f3249e0494fd5ad951ee1184fc944d7
Guilhermo <guilhermo@d2t.nl>
2023-07-11 16:35:17 +0200

    update 11-juli: UpdateProduct.js is updated met refresh function

--- src/components/adminComponents/updateProduct/UpdateProduct.js
+++ src/components/adminComponents/updateProduct/UpdateProduct.js
@@ -24,7 +24,7 @@ function UpdateProduct() {
     async function updateItemData(data) {
         toggleLoading(true);
         try {
-            await axios.put(`http://localhost:8080/products/${data.productId}`, {
+            await axios.put(`http://localhost:8080/products/${data.product_id}`, {
                 productId: data.product_id,
                 productName: data.product_name,
                 productDescription: data.product_description,
@@ -36,93 +36,98 @@ function UpdateProduct() {
                 }
             })
             toggleAddSuccess(true);
-            reset();
+            setTimeout(() => {
+                    refreshPage()
+                },
+                1000);
         } catch (e) {
             console.error(e);
         }
     }
 
+    function refreshPage() {
+        window.location.reload();
+    }
 
     return (<>
-            {user.roles !== "ROLE_ADMIN" ? <h3>Moet ingelogd zijn als Admin</h3> :
-                <div className="iu-container" id="update_product">
-                    <h2 className="uih-container">
-                        Product Aanpassen
-                    </h2>
-                    <section className="ui-container">
-                        <p>Pas hier je product</p>
-                        <RiErrorWarningLine size={25}/>
-                        <p>Alle velden moeten verplicht ingevuld worden!! </p>
-                    </section>
-                    <section>
-                        <form
-                            onSubmit={handleSubmit(updateItemData)}>
-                            <input
-                                type="text"
-                                id="product_id"
-                                {...register("product_id", {
-                                    required: "Product nummer is verplicht",
-                                })}
-                                placeholder="product nummer"
-                                autoComplete="off"
-                            />
-                            <p> {errors.product_id?.message}</p>
-                            <input
-                                type="text"
-                                id="product_name"
-                                placeholder="Product naam"
-                                {...register("product_name", {
-                                    required: "product naam is verplicht", pattern: /^[a-z ,.'-]+$/i,
-                                })}
-                                autoComplete="off"
-                            />
-                            <p> {errors.product_name?.message} </p>
+        {user.roles !== "ROLE_ADMIN" ? <h3>Moet ingelogd zijn als Admin</h3> :
+            <div className="iu-container" id="update_product">
+                <h2 className="uih-container">
+                    Product Aanpassen
+                </h2>
+                <section className="ui-container">
+                    <p>Pas hier je product</p>
+                    <RiErrorWarningLine size={25}/>
+                    <p>Alle velden moeten verplicht ingevuld worden!! </p>
+                </section>
+                <section>
+                    <form
+                        onSubmit={handleSubmit(updateItemData)}>
+                        <input
+                            type="text"
+                            id="product_id"
+                            {...register("product_id", {
+                                required: "Product nummer is verplicht",
+                            })}
+                            placeholder="product nummer"
+                            autoComplete="off"
+                        />
+                        <p> {errors.product_id?.message}</p>
+                        <input
+                            type="text"
+                            id="product_name"
+                            placeholder="Product naam"
+                            {...register("product_name", {
+                                required: "product naam is verplicht", pattern: /^[a-z ,.'-]+$/i,
+                            })}
+                            autoComplete="off"
+                        />
+                        <p> {errors.product_name?.message} </p>
 
 
-                            <textarea
-                                type="text"
-                                className="text-area-product"
-                                id="product_description"
-                                {...register("product_description", {
-                                    required: "product omschrijving is verplicht",
-                                })}
-                                rows={3}
-                                cols={20}
-                                placeholder="product omschrijving"
-                                autoComplete="off"
-                            />
-                            <p>{errors.product_description?.message}</p>
+                        <textarea
+                            type="text"
+                            className="text-area-product"
+                            id="product_description"
+                            {...register("product_description", {
+                                required: "product omschrijving is verplicht",
+                            })}
+                            rows={3}
+                            cols={20}
+                            placeholder="product omschrijving"
+                            autoComplete="off"
+                        />
+                        <p>{errors.product_description?.message}</p>
 
-                            <pre>Prijs in Euro...</pre>
-                            <input
-                                {...register("product_price", {
-                                    required: "product prijs is verplicht",
-                                })}
-                                placeholder="Product Prijs in €"
-                                type="number"
-                                id="product_price"
-                                min="1"
-                                step="0.10"
-                                title="Currency"
-                                lang="en-US"
-                                autoComplete="off"
-                            />
-                            <p>{errors.product_price?.message}</p>
+                        <pre>Prijs in Euro...</pre>
+                        <input
+                            {...register("product_price", {
+                                required: "product prijs is verplicht",
+                            })}
+                            placeholder="Product Prijs in €"
+                            type="number"
+                            id="product_price"
+                            min="1"
+                            step="0.10"
+                            title="Currency"
+                            lang="en-US"
+                            autoComplete="off"
+                        />
+                        <p>{errors.product_price?.message}</p>
 
-                            <div className="button-container">
-                                <button type="submit"
-                                        disabled={loading}
-                                >
-                                    Product bijwerken
-                                </button>
-                            </div>
-                            {addSuccess === true && <p>Gelukt met het creeren van een product. Ga naar mijn producten of
-                                <NavLink to="/admin">klik hier!</NavLink>
-                            </p>}
-                        </form>
-                    </section>
-                </div>}
-        </>)
+                        <div className="button-container">
+                            <button type="submit"
+                                    disabled={loading}
+                            >
+                                Product bijwerken
+                            </button>
+                        </div>
+                        {addSuccess === true && <p>Gelukt met het creeren van een product
+                        </p>}
+                    </form>
+                </section>
+            </div>}
+    </>)
 }
 
 export default UpdateProduct;
